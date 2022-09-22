64c6abaef8f5bb1860129f53b145f38dbcb7fb95
Akin GURLER <akingurler98@hotmail.com>
2022-09-22 22:57:59 +0300

    Video Detail page fetch video data view video player

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -10,7 +10,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => { //video/id/vide
             width:{md:"320px",xs:"%100"},
             boxShadow:"none", borderRadius:0
         }}>
-            <Link to={videoId ? `video/$videoID` : demoVideoUrl}>
+            <Link to={videoId ? `video/${videoId}` : demoVideoUrl}>
                 <CardMedia
                     image={snippet?.thumbnails?.high?.url}
                     alt={snippet?.title}
@@ -20,12 +20,12 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => { //video/id/vide
                   <CardContent sx={{ 
                     backgroundColor: "#1E1E1E", height: '106px'
                      }}>
-                        <Link to={videoId ? `video/$videoId` : demoVideoUrl}>
+                        <Link to={videoId ? `video/${videoId}` : demoVideoUrl}>
                             <Typography  variant="subtitle1" fontWeight="bold" color="#FFF"> 
                                 {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
                             </Typography>
                         </Link> 
-                        <Link to={channelId ? `channel/$channelId` : demoChannelUrl}>
+                        <Link to={channelId ? `channel/${channelId}` : demoChannelUrl}>
                             <Button sx={{marginY:1}} variant="outlined" color="error">
                             <Typography variant="subtitle2" fontWeight="bold" color="gray"> 
                                 {snippet?.channelTitle.slice(0,60) || demoChannelTitle.slice(0,60)}
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import {Link,useParams} from "react-router-dom"
+import ReactPlayer from "react-player"
+import {Typography,Box,Stack} from "@mui/material"
+
+import {Video} from "./"
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { ClassNames } from '@emotion/react'
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState([])
+  const {id}=useParams();
+  
+  console.log(id)
+  console.log(VideoDetail)
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet&id=${id}`)
+      .then((data)=>setVideoDetail(data.items[0]))
+      
+  
+    return () => {
+      
+    }
+  }, [id])
+
+  if(!videoDetail?.snippet) return "Loading..."
+  const {snippet} =videoDetail
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack>
+        <Box flex={1}>
+          <Box sx={{
+            width:"100%",
+            position:"sticky",
+            top:"86px"
+          }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}]`}
+            height="77vh" width="100%" controls/>
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {snippet.title}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+      
+    </Box>
   )
 }
 
